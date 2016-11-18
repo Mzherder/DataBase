4c6aeb7ec4902ad406530cdadb8ffb1676c5902a
MZHERDER <student@KIRP-405-STUD04>
2016-11-18 10:13:43 +0300

    Made request to the database

--- Sports.UI/Program.cs
+++ Sports.UI/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Sports.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,10 @@ namespace Sports.UI
     {
         static void Main(string[] args)
         {
+            using (Context context = new Context())
+            {
+                List<Team> list = context.Teams.ToList();
+            }
         }
     }
 }
